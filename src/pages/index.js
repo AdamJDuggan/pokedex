d116fcf4d12bec1a73bbfc3a95bb1c22d3aa9dac
AdamJDuggan <adamduggan17@gmail.com>
2021-08-17 15:15:46 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
+// React 
 import React from "react"
+// Components
+import Item from "./components/Item"
+// Styles
+
+import "../styles/index.scss"
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div className="bg-primary">Hello world!
+    <Item/>
+  </div>
 }
